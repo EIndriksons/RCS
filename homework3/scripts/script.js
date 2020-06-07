71b3d607f791cdbc41c276d6677c3e72b14dc5e8
EIndriksons <edgars@indriksons.lv>
2020-06-07 14:07:05 +0300

    Homework 3 - restructured files, added event handlers

---
+++ homework3/scripts/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // generate boxes
+    document.querySelector('#generate').onclick = () => {
+        
+    }
+
+    // clear boxes
+    document.querySelector('#clear').onclick = () => {
+        
+    }
+});
