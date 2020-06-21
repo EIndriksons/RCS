0a67bedb8107a8476027efd4fc690b21fecde023
Edgars Indriksons <edgars@indriksons.lv>
2020-06-21 23:16:57 +0300

    Reverted

--- homework4/resources/views/auth/login.blade.php
+++
@@ -1,9 +0,0 @@
-<form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-    {!! csrf_field() !!}
-
-    <input type="email" class="form-control" name="email">
-    <input type="password" class="form-control" name="password">
-    <input type="checkbox" name="remember"> Remember Me
-    <button type="submit" class="btn btn-primary">Login</button>
-
-</form>
